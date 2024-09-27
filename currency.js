7efc6fe607f8755822998cc94295045fc9e39615
pawanjagtap318 <pawanjagtap318@gmail.com>
2024-09-27 19:35:29 +0530

    change variable name

--- currency.js
+++ currency.js
@@ -5,7 +5,7 @@ const dropdowni = document.querySelector("i");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
-const msg = document.querySelector(".msg");
+const mesg = document.querySelector(".msg");
 
 for(let select of dropdowns) {
     for(currCode in countryList) {
